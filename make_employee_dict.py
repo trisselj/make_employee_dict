c7db14143c518bccba737ce9bc84ba75e14980a3
trisselj <trisselj@oregonstate.edu>
2024-08-07 12:29:58 -0700

    q

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -33,3 +33,5 @@ def make_employee_dict(_name, ID, Salary, Email):
         employee = Employee(name, ID, salary, email) # Creates the employee object
         employee_dict[ID] = employee # Adds the employee to the dictionary using ID as key
     return employee_dict
+
+# I cant find out why it doesnt like the way I attributed the name to the object, please give feedback explaining why, it is very much appreciated. Thanks!

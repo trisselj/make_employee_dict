7ef3311c241cd18e35cbbab849cf53d1e79024e3
trisselj <trisselj@oregonstate.edu>
2024-08-07 12:35:59 -0700

    q

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -29,8 +29,8 @@ def make_employee_dict(_name, ID, Salary, Email):
     employee_dict = {}
 
     # Loops through each index to creat the Employee objects
-    for name, ID, salary, email in zip(_name, ID, Salary, Email):
-        employee = Employee(name, ID, salary, email) # Creates the employee object
+    for name, ID_number, salary, email_address in zip(name, ID_number, salary, email_address):
+        employee = Employee(name, ID_number, salary, email_address) # Creates the employee object
         employee_dict[ID] = employee # Adds the employee to the dictionary using ID as key
     return employee_dict
 

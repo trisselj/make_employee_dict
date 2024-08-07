275f0a211c4513f807e07b3b4c05fae290f92697
trisselj <trisselj@oregonstate.edu>
2024-08-07 12:41:00 -0700

    q

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -6,23 +6,23 @@
 # Initializes data set with employee name, ID, salary, and email
 class Employee:
     def __init__(self, name, ID_number, salary, email_address): 
-        self.__name = name
-        self.__ID_number = ID_number
-        self.__salary = salary
-        self.__email_address = email_address
+        self._name = name
+        self._ID_number = ID_number
+        self._salary = salary
+        self._email_address = email_address
 
     # Returns name, ID, salary, and email
     def get_name(self):
-        return self.__name
+        return self._name
     
     def get_ID_number(self):
-        return self.__ID_number
+        return self._ID_number
     
     def get_salary(self):
-        return self.__salary
+        return self._salary
     
     def get_email_address(self):
-        return self.__email_address
+        return self._email_address
     
 # Creates empty dictionary for storing Employees
 def make_employee_dict(_name, ID, Salary, Email):
@@ -33,5 +33,3 @@ def make_employee_dict(_name, ID, Salary, Email):
         employee = Employee(name, ID, salary, email) # Creates the employee object
         employee_dict[ID] = employee # Adds the employee to the dictionary using ID as key
     return employee_dict
-
-# I cant find out why it doesnt like the way I attributed the name to the object, please give feedback explaining why, it is very much appreciated.

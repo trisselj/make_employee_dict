c27f461b8c32055a76e9cfa7b6b02365908f559e
trisselj <trisselj@oregonstate.edu>
2024-08-07 12:26:23 -0700

    q

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -25,11 +25,11 @@ class Employee:
         return self.__email_address
     
 # Creates empty dictionary for storing Employees
-def make_employee_dict(names, ids, salaries, emails):
+def make_employee_dict(_name, ID, Salary, Email):
     employee_dict = {}
 
     # Loops through each index to creat the Employee objects
-    for name, ID, salary, email in zip(names, ids, salaries, emails):
+    for name, ID, salary, email in zip(_name, ID, Salary, Email):
         employee = Employee(name, ID, salary, email) # Creates the employee object
         employee_dict[ID] = employee # Adds the employee to the dictionary using ID as key
     return employee_dict
